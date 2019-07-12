21080b4226e0fbac44625e535f38989fad7827d5
Daniel Sprague <danielsprague@Daniels-MacBook-Pro.local>
2019-07-12 13:58:43 -0400

    hmm

--- corefunctions.py
+++ corefunctions.py
@@ -75,27 +75,82 @@ def HMM(S,k,alphabet,O):
     #Reverse length normalization of seekr
     qUnNormCounts = q.counts.T*[len(s) for s in q.seqs]/1000
     qCounts = np.rint(qUnNormCounts.T)
-    hmmDict['model'] = qCounts/sum(qCounts)
-
+    qCounts+=1
+    qCounts = np.mean(qCounts,axis=0)
+    hmmDict['model'] = np.log2(qCounts/np.sum(qCounts))
     q = BasicCounter(null,k=k,mean=False,std=False,log2=False,alphabet=alphabet)
     q.get_counts()
     #Reverse length normalization of seekr
     qUnNormCounts = q.counts.T*[len(s) for s in q.seqs]/1000
     qCounts = np.rint(qUnNormCounts.T)
-    hmmDict['null'] = qCounts/sum(qCounts)
-
-
-    obs = [kmer[i:i+k] for i in range(len(O),len(O)-k+1)]
+    qCounts+=1
+    qCounts = np.mean(qCounts,axis=0)
+    #hmmDict['null'] = np.log2(qCounts/sum(qCounts))
+    hmmDict['null'] = np.log2(qCounts/np.sum(qCounts))
+    obs = [O[i:i+k] for i in range(0,len(O)-k+1)]
     states = ('Model','Null')
-    start_p = {'Model':.1,'Null':.9}
-    trans_p = {'Model':{'Model':.8,'Null':.2},'Null':{'Model':.1,'Null':.9}}
+    start_p = {'Model':np.log2(.5),'Null':np.log2(.5)}
+    trans_p = {'Model':{'Model':np.log2(.8),'Null':np.log2(.2)},'Null':{'Model':np.log2(.1),'Null':np.log2(.9)}}
     emit_p = {'Model': dict(zip(kmers,hmmDict['model'])),'Null':dict(zip(kmers,hmmDict['null']))}
     return obs,states,start_p,trans_p,emit_p
 
 def viterbi(obs,states,start_p,trans_p,emit_p):
-    return
+    V = [{}]
+    for st in states:
+        V[0][st] = {'prob':start_p[st]+emit_p[st][obs[0]],'prev':None}
+    for t in range(1,len(obs)):
+        V.append({})
+        for st in states:
+            max_tr_prob = V[t-1][states[0]]['prob']+trans_p[states[0]][st]
+            prev_st_selected = states[0]
+            for prev_st in states[1:]:
+                tr_prob = V[t-1][prev_st]['prob']+trans_p[prev_st][st]
+                if tr_prob > max_tr_prob:
+                    max_tr_prob = tr_prob
+                    prev_st_selected = prev_st
+            max_prob = max_tr_prob + emit_p[st][obs[t]]
+            V[t][st] = {'prob':max_prob,'prev':prev_st_selected}
 
-def dptable(V):
-    yield " ".join(("%12d" % i) for i in range(len(V)))
-    for state in V[0]:
-        yield "%.7s: " % state + " ".join("%.7s" % ("%f" % v[state]["prob"]) for v in V)
+    opt = []
+    max_prob = max(value['prob'] for value in V[-1].values())
+    previous = None
+    for st,data in V[-1].items():
+        if data['prob'] == max_prob:
+            opt.append(st)
+            previous = st
+            break
+
+    for t in range(len(V)-2,-1,-1):
+        opt.insert(0,V[t+1][previous]['prev'])
+        previous = V[t+1][previous]['prev']
+    return opt
+    # uk=[{}]
+    # ukprev = [{}]
+    # N = len(obs)
+    # print(N)
+    # for state in states:
+    #     uk[0][state]=start_p[state]+emit_p[state][obs[0]]
+    #     ukprev[0][state] = None
+    # for n in range(1,N):
+    #     uk.append({})
+    #     ukprev.append({})
+    #     for state in states:
+    #         prevSelState = states[0]
+    #         currMaxProb = emit_p[state][obs[n]]+trans_p[state][prevSelState] + uk[n-1][state]
+    #         for pState in states[1:]:
+    #             currProb = emit_p[state][obs[n]]+trans_p[state][pState] + uk[n-1][state]
+    #             if currProb > currMaxProb:
+    #                 currMaxProb = currProb
+    #                 prevSelState = pState
+    #         max_prob = currMaxProb + emit_p[state][obs[n]]
+    #         uk[n][state] = max_prob
+    #         ukprev[n][state] = prevSelState
+    # z = max(uk[-1],key=uk[-1].get)
+    # prev = ukprev[-1][z]
+    # backtrack = [z,prev]
+    # for n in range(N-2,-1,-1):
+    #     z = max(uk[-n],key=uk[-n].get)
+    #     prev = ukprev[-n][z]
+    #     backtrack.append(prev)
+    # backtrack = backtrack[::-1]
+    return obs, backtrack
