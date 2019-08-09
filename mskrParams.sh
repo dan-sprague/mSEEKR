8c47289086189ec94bdd4ab4bb00bf27028372e3
Daniel Sprague <danielsprague@daniels-mbp.wireless-1x.unc.edu>
2019-08-09 15:17:06 -0400

    update

--- mskrParams.sh
+++ mskrParams.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
+
 for w in 50 100 200 500
 do
   for k in 2 3 4 5
   do
     for c in AE BCD
     do
-      python mSEEKR.py --model ./markovModels/mam${c}_hg26Trscpt_${k}.mkv --db ./fastaFiles/okRsx.fa --prefix rsx -w $w
+      python mSEEKR.py --model ./markovModels/mam${c}_hg26Trscpt_${k}.mkv --db ./fastaFiles/oRsx.fa --prefix orsx -w $w
+      python mSEEKR.py --model ./markovModels/mam${c}_hg26Trscpt_${k}.mkv --db ./fastaFiles/kRsx.fa --prefix krsx -w $w
     done
   done
 done
