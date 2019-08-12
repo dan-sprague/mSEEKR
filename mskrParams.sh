6e8e7e36ca783611ac0e05f0589239cabf32f7ae
Daniel Sprague <danielsprague@Daniels-MacBook-Pro.local>
2019-08-12 11:43:32 -0400

    agg

--- mskrParams.sh
+++ mskrParams.sh
@@ -2,12 +2,19 @@
 
 for w in 50 100 200 500
 do
-  for k in 2 3 4 5
+  for k in 2 3 4 5 6
   do
     for c in AE BCD
     do
       python mSEEKR.py --model ./markovModels/mam${c}_hg26Trscpt_${k}.mkv --db ./fastaFiles/oRsx.fa --prefix orsx -w $w
       python mSEEKR.py --model ./markovModels/mam${c}_hg26Trscpt_${k}.mkv --db ./fastaFiles/kRsx.fa --prefix krsx -w $w
+      python mSEEKR.py --model ./markovModels/noHuman${c}_hg26Trscpt_${k}.mkv --db ./fastaFiles/mamX/human.fa --prefix human -w $w
+      python mSEEKR.py --model ./markovModels/noMouse${c}_hg26Trscpt_${k}.mkv --db ./fastaFiles/mamX/mouse.fa --prefix mouse -w $w
+      python mSEEKR.py --model ./markovModels/noMarm${c}_hg26Trscpt_${k}.mkv --db ./fastaFiles/mamX/marm.fa --prefix marmoset -w $w
+      python mSEEKR.py --model ./markovModels/noDog${c}_hg26Trscpt_${k}.mkv --db ./fastaFiles/mamX/dog.fa --prefix dog -w $w
+      python mSEEKR.py --model ./markovModels/noCow${c}_hg26Trscpt_${k}.mkv --db ./fastaFiles/mamX/cow.fa --prefix cow -w $w
+      python mSEEKR.py --model ./markovModels/noRat${c}_hg26Trscpt_${k}.mkv --db ./fastaFiles/mamX/rat.fa --prefix rat -w $w
+      python mSEEKR.py --model ./markovModels/noPig${c}_hg26Trscpt_${k}.mkv --db ./fastaFiles/mamX/pig.fa --prefix pig -w $w
     done
   done
 done
--- runParams.sh
+++ runParams.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-for c in noDog noRat noPig noHuman noMouse noBoar noMarm noCow
+for c in noDog noRat noPig noHuman noMouse noBoar noMarm noCow mam
 do
   for d in AE BCD
   do
-    python train.py --query ./counts/${c}${d}.skr --null ./counts/hg26Transcripts.skr -k 2,3,4,5 --qPrefix ${c}${d} --nullPrefix hg26Trscpt
+    python train.py --query ./counts/${c}${d}.skr --null ./counts/hg26Transcripts.skr -k 2,3,4,5,6 --qPrefix ${c}${d} --nullPrefix hg26Trscpt
   done
 done
