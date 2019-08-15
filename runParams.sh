fc8ad298ed2743c3252edc2e03e099bc177c1fd3
Daniel Sprague <danielsprague@Daniels-MacBook-Pro.local>
2019-08-15 12:19:31 -0400

    update

--- runParams.sh
+++ runParams.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-for c in noDog noRat noPig noHuman noMouse noBoar noMarm noCow mam
+for c in noDog noRat noPig noHuman noMouse noMarm noCow mam
 do
-  for d in AE BCD
+  for d in A B D E
   do
     python train.py --query ./counts/${c}${d}.skr --null ./counts/hg26Transcripts.skr -k 2,3,4,5,6 --qPrefix ${c}${d} --nullPrefix hg26Trscpt
   done
