e74e566ffb6d29fa234c808e80ca7dcf05bd5c4f
Dan Sprague <spragud@gmail.com>
2020-06-25 10:36:41 -0400

    Comments and explanations

--- bw.py
+++ bw.py
@@ -74,7 +74,7 @@ for i in tqdm(range(args.its)):
     # That is, for prior state "i" (can be either query or null) transitioning to either query (+) or null (-): p(-|i) + p(+|j) != 1 
     # Here, we calculate the marginal probability for transitioning from state i to either state i or j (normalizing constant)
     # Then, divide each probability p(-|i) and p(+|i) by the marginal probability 
-    # Now, p(-|i) + p(+|j) = 1
+    # Now, p(-|i) + p(+|i) = 1
     for i in states:
         # marginal probability (normalizing constant)
         marg = logsumexp(list(A[i].values())) # cannot simply sum in log-space, logsumexp to sum log-space numbers without over/under-flow (see wiki page)
